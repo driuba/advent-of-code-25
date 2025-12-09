3c004afbaef517bccb2945aa4062b6100bcf1ce0
Andrius Andrikonis <andrius.andrikonis@toughlex.com>
2025-12-09 18:30:01 +0200

    day 9 part 2 progress

--- day-9/maximizer.py
+++ day-9/maximizer.py
@@ -4,6 +4,54 @@ from math import inf
 from sys import argv
 
 
+def cross(a, b):
+	return a[0] * b[1] - a[1] * b[0]
+
+
+def find_winding(points, normals):
+	x_min = inf
+	x_max = -inf
+	y_min = inf
+	y_max = -inf
+
+	for (x, y) in points:
+		if x_min > x:
+			x_min = x
+
+		if x_max < x:
+			x_max = x
+
+		if y_min > y:
+			y_min = y
+
+		if y_max < y:
+			y_max = y
+
+	for (index, (x, y)) in enumerate(points):
+		if not (x == x_min or x == x_max or y == y_min or y == y_max):
+			continue
+
+		normal = normals[index]
+
+		if normal:
+			return normal // abs(normal)
+
+	return 0
+
+
+def get_vectors(points):
+	def iterate():
+		(x1, y1) = points[-1]
+		(x2, y2) = points[0]
+
+		yield (x2 - x1, y2 - y1)
+
+		for ((x1, y1), (x2, y2)) in zip(points[:-1], points[1:]):
+			yield (x2 - x1, y2 - y1)
+
+	return list(iterate())
+
+
 def main():
 	count = len(argv) - 1
 
@@ -21,12 +69,32 @@ def process(filename):
 				yield areas[row][column]
 
 	tiles = read_file(filename)
+	steps = get_vectors(tiles)
+
+	print(*tiles)
+	print(*steps)
+
+	directions = []
+
+	for (a, b) in zip(steps[:-1], steps[1:]):
+		directions.append(cross(a, b))
+	else:
+		directions.append(cross(b, steps[0]))
+
+	print(directions)
+
+	print(find_winding(tiles, directions))
+
+	return
 
 	areas = [[1 if i == j else -inf for j in tiles] for i in tiles]
 
 	for row in range(len(tiles) - 1):
 		for column in range(row + 1, len(tiles)):
-			areas[row][column] = areas[column][row] = (abs(tiles[row][0] - tiles[column][0]) + 1) * (abs(tiles[row][1] - tiles[column][1]) + 1)
+			(x1, y1) = tiles[row]
+			(x2, y2) = tiles[column]
+
+			areas[row][column] = areas[column][row] = (abs(x1 - x2) + 1) * (abs(y1 - y2) + 1)
 
 	print(max(iterate(areas)))
 
@@ -38,7 +106,9 @@ def read_file(filename):
 				line = line.strip()
 
 				if line:
-					yield tuple(int(c) for c in line.split(','))
+					[x, y] = line.split(',')
+
+					yield (int(x), int(y))
 
 	return list(read())
 
