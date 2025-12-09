a20665331361c86a91545ab1c6da5a32386e8e88
Andrius Andrikonis <andrikonis.andrius@gmail.com>
2025-12-09 22:02:41 +0200

    day 9 part 2

--- day-9/maximizer.py
+++ day-9/maximizer.py
@@ -1,5 +1,6 @@
 #!/bin/env python3.14t
 
+
 from math import inf
 from sys import argv
 
@@ -8,13 +9,44 @@ def cross(a, b):
 	return a[0] * b[1] - a[1] * b[0]
 
 
-def find_winding(points, normals):
+def find_max_area(tiles):
+	def iterate_invalid():
+		for (i, (_, n)) in enumerate(tiles):
+			if n < 0:
+				((x1, y1), _) = tiles[(i - 1) % len(tiles)]
+				((x2, y2), _) = tiles[(i + 1) % len(tiles)]
+
+				yield ((min(x1, x2), min(y1, y2)), (max(x1, x2), max(y1, y2)))
+
+	ranges_invalid = list(iterate_invalid())
+
+	result = -inf
+
+	for (index, ((x1_initial, y1_initial), _)) in enumerate(tiles):
+		for ((x2_initial, y2_initial), _) in tiles[index:]:
+			x1 = min(x1_initial, x2_initial)
+			y1 = min(y1_initial, y2_initial)
+			x2 = max(x1_initial, x2_initial)
+			y2 = max(y1_initial, y2_initial)
+
+			if any((x1 < x2_i and y1 < y2_i and x2 > x1_i and y2 > y1_i for ((x1_i, y1_i), (x2_i, y2_i)) in ranges_invalid)):
+				continue
+
+			area = (abs(x1 - x2) + 1) * (abs(y1 - y2) + 1)
+
+			if result < area:
+				result = area
+
+	return result
+
+
+def find_winding(points):
 	x_min = inf
 	x_max = -inf
 	y_min = inf
 	y_max = -inf
 
-	for (x, y) in points:
+	for ((x, y), _) in points:
 		if x_min > x:
 			x_min = x
 
@@ -27,16 +59,26 @@ def find_winding(points, normals):
 		if y_max < y:
 			y_max = y
 
-	for (index, (x, y)) in enumerate(points):
+	for ((x, y), normal) in points:
 		if not (x == x_min or x == x_max or y == y_min or y == y_max):
 			continue
-
-		normal = normals[index]
 
 		if normal:
 			return normal // abs(normal)
 
 	return 0
+
+
+def get_normals(points):
+	vectors = get_vectors(points)
+
+	def iterate():
+		for (a, b) in zip(vectors[:-1], vectors[1:]):
+			yield cross(a, b)
+
+		yield cross(vectors[-1], vectors[0])
+
+	return list(iterate())
 
 
 def get_vectors(points):
@@ -63,40 +105,14 @@ def main():
 
 
 def process(filename):
-	def iterate(areas):
-		for row in range(len(areas)):
-			for column in range(row, len(areas[row])):
-				yield areas[row][column]
-
 	tiles = read_file(filename)
-	steps = get_vectors(tiles)
+	tiles = list(zip(tiles, get_normals(tiles)))
 
-	print(*tiles)
-	print(*steps)
+	winding = find_winding(tiles)
 
-	directions = []
+	tiles = [(p, n * winding) for (p, n) in tiles]
 
-	for (a, b) in zip(steps[:-1], steps[1:]):
-		directions.append(cross(a, b))
-	else:
-		directions.append(cross(b, steps[0]))
-
-	print(directions)
-
-	print(find_winding(tiles, directions))
-
-	return
-
-	areas = [[1 if i == j else -inf for j in tiles] for i in tiles]
-
-	for row in range(len(tiles) - 1):
-		for column in range(row + 1, len(tiles)):
-			(x1, y1) = tiles[row]
-			(x2, y2) = tiles[column]
-
-			areas[row][column] = areas[column][row] = (abs(x1 - x2) + 1) * (abs(y1 - y2) + 1)
-
-	print(max(iterate(areas)))
+	print(find_max_area(tiles))
 
 
 def read_file(filename):
