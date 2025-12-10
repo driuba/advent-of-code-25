8e8ea4ae1d9094c8ca66620c4a1932abb2b6a0d1
Andrius Andrikonis <andrius.andrikonis@toughlex.com>
2025-12-10 18:02:35 +0200

    day 10 part 2 progress

--- day-10/initializer.py
+++ day-10/initializer.py
@@ -2,44 +2,88 @@
 
 
 from collections import deque
+from math import sqrt
 from sys import argv
 
 
-def find_path(machine):
-	(state_target, buttons, _) = machine
+def find_joltage_path(machine):
+	def distance(joltage_candidate, joltage_target):
+		return sqrt(sum((jc - jt) ** 2 for (jc, jt) in zip(joltage_candidate, joltage_target)))
 
-	queue = deque()
+	def iterate_buttons(joltage, joltage_target, buttons):
+		for button in buttons:
+			joltage_candidate = tuple(j + 1 if i in button else j for (i, j) in enumerate(joltage))
 
-	queue.append((tuple(False for _ in state_target), [], buttons))
+			if any(jc > jt for (jc, jt) in zip(joltage_candidate, joltage_target)):
+				continue
 
-	while len(queue):
-		(state, steps, buttons) = queue.popleft()
+			yield (joltage_candidate, button)
 
-		if state == state_target:
-			return steps
+	try:
+		(_, buttons, joltage_target) = machine
 
-		for (index, button) in enumerate(buttons):
-			queue.append((
-				tuple(not s if i in button else s for (i, s) in enumerate(state)),
-				[*steps, button],
-				buttons[index + 1:]
-			))
+		queue = deque()
+
+		queue.append((tuple(0 for _ in joltage_target), [], buttons))
+
+		while len(queue):
+			(joltage, steps, buttons) = queue.popleft()
+
+			print(joltage_target, joltage, end='\r')
+
+			if joltage == joltage_target:
+				return steps
+
+			for (joltage_candidate, button) in sorted(iterate_buttons(joltage, joltage_target, buttons), key=lambda b: distance(b[0], joltage_target)):
+				queue.append((joltage_candidate, [*steps, button], buttons))
+	finally:
+		print()
+
+
+def find_state_path(machine):
+	try:
+		(state_target, buttons, _) = machine
+
+		queue = deque()
+
+		queue.append((tuple(False for _ in state_target), [], buttons))
+
+		while len(queue):
+			(state, steps, buttons) = queue.popleft()
+
+			print(state_target, state, end='\r')
+
+			if state == state_target:
+				return steps
+
+			for (index, button) in enumerate(buttons):
+				queue.append((
+					tuple(not s if i in button else s for (i, s) in enumerate(state)),
+					[*steps, button],
+					buttons[index + 1:]
+				))
+	finally:
+		print()
 
 
 def main():
 	count = len(argv) - 1
 
-	assert count <= 1, 'More than one argument is not supported.'
+	assert count <= 2, 'More than two arguments are not supported.'
 
 	filename = argv[1] if count >= 1 else 'input.txt'
+	joltage = count >= 2
 
-	process(filename)
+	process(filename, joltage)
 
 
-def process(filename):
+def process(filename, joltage):
 	machines = read_file(filename)
 
-	print(sum((len(find_path(m)) for m in machines)))
+	if joltage:
+		print(sum((len(find_joltage_path(m)) for m in machines)))
+	else:
+		print(sum((len(find_state_path(m)) for m in machines)))
 
 
 def read_file(filename):
