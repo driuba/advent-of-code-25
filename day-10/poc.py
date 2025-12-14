bab968dec31a44e58d2e886c766d82f65a9cdbd4
Andrius Andrikonis <andrikonis.andrius@gmail.com>
2025-12-15 00:32:02 +0200

    WIP: day 10 part 2
    still kicking my ass
    added fractional support and variable substitution step
    still need to figure out a good way to check basic solutions
    programatically

--- day-10/poc.py
+++ day-10/poc.py
@@ -1,26 +1,27 @@
 #!/bin/env python3.14t
 
 
+from fractions import Fraction
 from itertools import product
-from math import inf
+from math import inf, lcm
 from sys import argv
 
 
 def analyze(_, buttons, joltages):
 	try:
-		matrix = [[1 if i in b else 0 for i in range(len(joltages))] for b in buttons]
+		matrix = [[Fraction(1) if i in b else Fraction(0) for i in range(len(joltages))] for b in buttons]
 		matrix = transpose(matrix)
-		matrix = [(*mr, r) for (mr, r) in zip(matrix, joltages)]
+		matrix = [(*mr, Fraction(r)) for (mr, r) in zip(matrix, joltages)]
 
 		index_row = -1
 
 		while True:
-			matrix.sort(key=lambda mr: min((i for (i, mc) in enumerate(mr) if mc), default=inf))
-
 			index_row += 1
 
 			if index_row == len(matrix):
 				break
+
+			matrix[index_row:] = sorted(matrix[index_row:], key=lambda mr: min((i for (i, mc) in enumerate(mr) if mc), default=inf))
 
 			row_current = matrix[index_row]
 			index_column = min((i for (i, cc) in enumerate(row_current) if cc), default=-1)
@@ -29,39 +30,83 @@ def analyze(_, buttons, joltages):
 				continue
 
 			column_current = row_current[index_column]
-			matrix[index_row] = row_current = tuple(c // column_current for c in row_current)
+			matrix[index_row] = row_current = tuple(c / column_current for c in row_current)
 
 			for index in range(index_row + 1, len(matrix)):
 				row = matrix[index]
 
-				multiplier = row[index_column] // column_current
+				multiplier = row[index_column]
 
 				if not multiplier:
 					continue
 
 				matrix[index] = tuple(c - multiplier * cc for (c, cc) in zip(row, row_current))
 
-		matrix = list({r for r in matrix if any(r)})
+		matrix = [r for r in matrix if any(r)]
+
+		#print(*matrix, sep='\n', end='\n\n')
+
+		running = True
+		substitutions_processed = set()
+
+		while running:
+			running = False
+
+			substitutions = [r for r in matrix if r not in substitutions_processed and count(c for c in r[:-1] if c) == 1]
+
+			for substitution in substitutions:
+				substitutions_processed.add(substitution)
+
+				running = True
+
+				index_column = min(i for (i, s) in enumerate(substitution) if s)
+
+				for index_row in range(len(matrix)):
+					row = matrix[index_row]
+
+					if substitution == row:
+						continue
+
+					multiplier = row[index_column]
+
+					if not multiplier:
+						continue
+
+					matrix[index_row] = tuple(c - multiplier * sc for (c, sc) in zip(row, substitution))
+
+		for index in range(len(matrix)):
+			multiplier = lcm(*(c.denominator for c in matrix[index] if c))
+
+			matrix[index] = (c * multiplier for c in matrix[index])
+			matrix[index] = tuple(c.numerator if c.is_integer() else c for c in matrix[index])
 
 		print(*matrix, sep='\n', end='\n\n')
 
-		# TODO: run substitutions if any, same idea as with previous while loop but this time work with lines that have only one variable and subtract that from every other row, repeat until no more substitutions are possible
+		bounds = get_bounds(matrix)
 
-		#bounds = get_bounds(matrix, joltages)
-
-		#print(*bounds, end='\n\n')
+		print(*bounds, end='\n\n')
 
 		#equations = get_equations(matrix, joltages)
 
-		#print(min((sum(vs) for vs in product(*(range(b, -1, -1) for b in bounds)) if all((e(vs) for e in equations)))))
+		#print(min((sum(vs) for vs in product(*(range(*b) for b in bounds)) if all((e(vs) for e in equations)))))
 	finally:
 		print()
 
 
-def get_bounds(matrix, results):
-	bounds = [[mc * r for mc in mr] for (mr, r) in zip(transpose(matrix), results)]
+def count(iterable):
+	result = 0
 
-	return [min((c for c in r if c)) for r in transpose(bounds)]
+	for _ in iterable:
+		result += 1
+
+	return result
+
+
+def get_bounds(matrix):
+	bounds = transpose([[c * r[-1] for c in r[:-1]] for r in matrix])
+	bounds = [min((c for c in r if c > 0), default=0) for r in bounds]
+
+	return bounds
 
 
 def get_equations(matrix, results):
@@ -71,7 +116,7 @@ def get_equations(matrix, results):
 
 		return check
 
-	return [get_check(vs, r) for (vs, r) in zip(transpose(matrix), results)]
+	return [get_check(r[:-1], r[-1]) for r in matrix]
 
 
 def main():
